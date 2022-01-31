26faf2976d673fbe4be7cb5845aff4ff802f625e
AtariAmiga <atarmigatar@gmail.com>
2022-01-31 13:28:36 +0100

    Agenda generator: dates in French

--- agenda.py
+++ agenda.py
@@ -1,4 +1,6 @@
 # pip install reportlab
+import datetime
+import locale
 
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
@@ -9,21 +11,29 @@ c = canvas.Canvas("hello.pdf", pagesize=A4_landscape)
 width = A4_landscape[0]
 height = A4_landscape[1]
 
+locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
+now = datetime.datetime.today()
+
 col_width = width / 3.0
+margin = 5
 for i in range(1, 3):
     x = i * col_width
-    c.line(x, 0, x, height)
+    c.line(x, margin, x, height - margin)
 
-nbr_lines = 20
+nbr_lines = 30
 top = 50
 line_height = (height - top) / nbr_lines
+matter_ratio = 5
+
+c.setLineWidth(0.1)
 for i in range(0, 3):
     x = i * col_width
-    c.drawCentredString(x + col_width/2, height - top, "Hello World")
-    for j in range(0, nbr_lines):
+    date = (now + datetime.timedelta(days=i)).strftime('%A %d %B %Y') # https://docs.python.org/fr/3.6/library/datetime.html#strftime-strptime-behavior
+    c.drawCentredString(x + col_width/2, height - top, date)
+    for j in range(1, nbr_lines):
         y = line_height * j
-        c.line(x + 5, y, x + col_width/4 - 2, y)
-        c.line(x + col_width/4 + 2, y, x + col_width - 5, y)
+        c.line(x + margin, y, x + col_width / matter_ratio - 2, y)
+        c.line(x + col_width / matter_ratio + 2, y, x + col_width - margin, y)
 
 
 c.showPage()
