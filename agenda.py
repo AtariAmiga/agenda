7de25793c458ce8c22812bec3ab37379f53a71c5
AtariAmiga <atarmigatar@gmail.com>
2022-01-31 13:19:52 +0100

    Agenda generator: horizontal lines

--- agenda.py
+++ agenda.py
@@ -14,9 +14,17 @@ for i in range(1, 3):
     x = i * col_width
     c.line(x, 0, x, height)
 
+nbr_lines = 20
+top = 50
+line_height = (height - top) / nbr_lines
 for i in range(0, 3):
     x = i * col_width
-    c.drawCentredString(x + col_width/2, height - 50, "Hello World")
+    c.drawCentredString(x + col_width/2, height - top, "Hello World")
+    for j in range(0, nbr_lines):
+        y = line_height * j
+        c.line(x + 5, y, x + col_width/4 - 2, y)
+        c.line(x + col_width/4 + 2, y, x + col_width - 5, y)
+
 
 c.showPage()
 c.save()
